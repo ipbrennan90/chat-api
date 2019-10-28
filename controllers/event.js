e959f7dd6800bc11d42b90256180cdb4ddaa5c0a
Ian Brennan <ian.brennan@viget.com>
2019-10-28 10:01:58 -0500

    Simple Event Model Init
    * Added Event to database models with migration and model file
    
    * Added post endpoint to create events
    
    * Added EventsController and create action
    
    * Added tests for EventController create action

---
+++ controllers/event.js
@@ -0,0 +1,24 @@
+import { Event } from '../database/models'
+
+const EventController = {
+  create: async (req, res) => {
+    const data = {
+      user: req.body.user,
+      type: req.body.type.toUpperCase(),
+      date: new Date(req.body.date),
+      message: req.body.message,
+      otheruser: req.body.otheruser
+    }
+    try {
+      await Event.create(data)
+      return res.status(200).json({
+        status: 'ok'
+      })
+    } catch (e) {
+      console.warn(e)
+      return res.status(500).json({ status: 'error' })
+    }
+  }
+}
+
+export default EventController
